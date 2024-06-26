0d517f0c450efb9d8b7b828f34aab28dbf38e611
Gian <gianc.ps.19@outlook.com>
2024-06-26 01:41:24 -0500

    agregando contenido

--- src/components/SecProductos.jsx
+++ src/components/SecProductos.jsx
@@ -10,14 +10,29 @@ function SecProductos() {
                 precio={30}
             />
             <Productos
-                imagen="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRus06COvKWEPeiQUHxdplUQE4aPbeWO1J1Jg&s"
+                imagen="https://abucow.com/cdn/shop/files/2_bc946045-d723-4dcc-890a-9155aa5d7c9b.jpg?v=1699866215"
                 nombre="Teclado mecanico"
                 precio={50}
             />
             <Productos
-                imagen="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRthoTYCn66nwQYLnX4Tp48BSd--hiigjBY7g&s"
+                imagen="https://promart.vteximg.com.br/arquivos/ids/6561757-1000-1000/image-dc319344bcb84db59893e8c374dc687c.jpg?v=638007866111200000"
                 nombre="Juguete para perros"
                 precio={10}
+            />
+            <Productos
+                imagen="https://dermashop.pe/cdn/shop/files/eucerin-sun-1-1.png?v=1704474125&width=1000"
+                nombre="Bloqueador solar"
+                precio={15}
+            />
+            <Productos
+                imagen="https://rimage.ripley.com.pe/home.ripley/Attachment/MKP/2269/PMP00002010255/Image-1.png"
+                nombre="Pelota de basket"
+                precio={40}
+            />
+            <Productos
+                imagen="https://dcdn.mitiendanube.com/stores/001/334/030/products/classic_classic_morsa1-6e920fd541eea69a2515994505146153-240-0.png"
+                nombre="Lampara de escritorio"
+                precio={25}
             />
         </div>
     </div>
