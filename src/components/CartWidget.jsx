8ea6d9c6d0ef2d279772231192d01af3540a6a8d
Gian <gianc.ps.19@outlook.com>
2024-06-26 20:44:03 -0500

    corrigiendo carrito

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,6 +1,9 @@
 function Carrito (){
     return(
+        <>
         <img src="https://cdn-icons-png.flaticon.com/512/3144/3144456.png" alt="carrito" id="carrito" href="#"/>
+        0
+        </>
     )
 }
 export default Carrito;
